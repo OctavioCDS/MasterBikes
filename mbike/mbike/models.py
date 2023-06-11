71bca0615e21fd045c4e7e39a9dc269b14ab7a06
Sebastián G <seba.gallardog@duocuc.cl>
2023-06-11 18:48:18 -0400

    modelo producto

--- mbike/mbike/models.py
+++ mbike/mbike/models.py
@@ -1,19 +1,16 @@
 from django.db import models
 
 
-# Crea el modelo producto
-
-
 class Producto(models.Model):
-
-    class meta:
+    class Meta:
         app_label = 'mbike'
 
     nombre = models.CharField(max_length=50)
-    descripcion = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=500)
     precio = models.IntegerField()
     stock = models.IntegerField()
     imagen = models.BinaryField()
+    marca = models.CharField(max_length=50)
 
     def __str__(self):
         return self.nombre
